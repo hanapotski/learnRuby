c1d55c32750291bd0b05201070d12061d796ed49
hanapotski <hannahjoy.zulueta@gmail.com>
2017-07-29 21:44:39 -0700

    chapter 2

---
+++ codecademy/2.14.rb
@@ -0,0 +1,10 @@
+x = 10
+y = 15
+
+if x < y
+  print "x is less than y"
+elsif x > y
+  print "x is greater than y"
+else
+  print "x is equal to y"
+end
---
+++ codecademy/2.15.rb
@@ -0,0 +1,7 @@
+bored = false
+
+unless bored
+  print "I'm excited to learn Ruby!"
+else
+  print "I'm bored"
+end

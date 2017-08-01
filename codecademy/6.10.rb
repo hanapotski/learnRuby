a23c31ed7405c72868f4ff82b125fbb9cdda7717
hanapotski <hannahjoy.zulueta@gmail.com>
2017-08-01 14:17:15 -0700

    chapter6

---
+++ codecademy/6.10.rb
@@ -0,0 +1,10 @@
+strings = ["HTML", "CSS", "JavaScript", "Python", "Ruby"]
+
+# Add your code below!
+symbols = []
+
+strings.each do |s|
+  symbols.push(s.intern)
+end
+
+print symbols
---
+++ codecademy/6.12.rb
@@ -0,0 +1,4 @@
+movies = {
+  beauty_and_the_beast: "very good",
+  wonderwoman: "amazing",
+  }

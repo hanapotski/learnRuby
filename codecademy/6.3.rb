cf05a6dc35de6484438c9446d9d2aee8e7617600
hanapotski <hannahjoy.zulueta@gmail.com>
2017-07-31 22:43:11 -0700

    chapter6

---
+++ codecademy/6.3.rb
@@ -0,0 +1,11 @@
+# Define your method below!
+def greeting
+  puts "How are you today?"
+end
+
+
+
+# Define your method above this line.
+
+greeting # Ignore this for now. We'll explain
+         # it in the next exercise!
